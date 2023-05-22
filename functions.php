9357806ac07739e767d112f2ccbddf445dcffd01
misgana96 <misganayoseph@gmail.com>
2023-05-22 11:46:44 +0300

    add woocommerece review display name

--- functions.php
+++ functions.php
@@ -222,11 +222,13 @@ function comments_filter_uprise( $author = '' ) {
     if ( !empty($comment->comment_author_email) ) {
         if (!empty($comment->comment_author_email)){
             $user=get_user_by('email', $comment->comment_author_email);
-//          $author=substr($user->first_name,0,1).' '. $user->last_name.'';
-            $author = trim($comment->comment_author);
-            $last_name = (strpos($author, ' ') === false) ? '' : preg_replace('#.*\s([\w-]*)$#', '$1', $author);
-            $first_name = trim( preg_replace('#'.preg_quote($last_name,'#').'#', '', $author ) );
-            $author= $first_name;
+			$author = trim($comment->comment_author);
+			$last_name = (strpos($author, ' ') === false) ? '' : preg_replace('#.*\s([\w-]*)$#', '$1', $author);
+			$first_name = trim( preg_replace('#'.preg_quote($last_name,'#').'#', '', $author ) );
+			$add_last_name_first_letter = $first_name === '' ? '' : ' ' . esc_html(substr($last_name,0,1));
+			$add_dot = (substr($last_name,0,1) === '' or $first_name === '') ? '' : '.';
+			$add_S = $first_name === 'Dr. Alex'? esc_html( substr( "'s", 0, 2) ) : $add_last_name_first_letter . $add_dot;
+			$author = $first_name === '' ? $last_name . $add_S : $first_name . $add_S;
         } else {
             $user = get_user_by( 'email', $comment->comment_author_email );
             $author = $user->first_name;
